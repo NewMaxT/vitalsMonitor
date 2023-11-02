b05aa3c6b6c9bda8e5f159a28023c59129cc2e6e
Maxence Goutteratel <maxencegoutteratel@gmail.com>
2023-11-01 23:42:57 -0400

    Major changes
    - Using Rider IDE with .sln visual project file
    - Adding a data sample to use during debug or development stages
    - Enhancing methods and structs
    - Graph class
    - Better comments
    - More TODOs
    - Adding enums for hard coded vars
    - Following C++ 23 guidelines

--- vitalsMonitor/Sensors.h
+++ vitalsMonitor/Sensors.h
@@ -1,25 +1,28 @@
 #pragma once
+#include <vector>
 
 
 #ifdef __linux__ 
 	// Linux Code
 #elif _WIN32
+	/*
+			@brief Sensors Class.
+			@brief Handles the hardware sensors and data stream.
+	*/
 	class Sensors
 	{
 	private:
 		/*
-		 * @brief Stores Raw Data from sensors
-		 * var: _rawData (Array short int)
-		 * [0] = HRM Sensor raw value, [1] = O2Sat Sensor raw value, [2] = RR Sensor raw value
+		 * @brief Stores Raw Data from sensors.
+		 * @brief [0] = HRM Sensor raw value, [1] = O2Sat Sensor raw value, [2] = RR Sensor raw value.
 		 */
 		short int _rawData[3];
 
 
 		/*
-		 * @brief Start Bluetooth Low Energy Service
-		 *
-		 * Use the interface to make a BLE API Object
-		 * Windows BLE API: https://github.com/urish/win-ble-cpp/tree/master
+		 * @brief Start Bluetooth Low Energy Service.
+		 * @brief Use the interface to make a BLE API Object.
+		 * @see Windows BLE API: https://github.com/urish/win-ble-cpp/tree/master
 		 */
 		void init_BLE();
 
@@ -28,21 +31,21 @@
 
 	public:
 		/*
-			 function: Sensors()
-			 use case: Constructor of Sensors Class
+			 @brief Constructor of Sensors Class.
+			 @brief Creates an object of the class Sensors.
 
-			 vars: isHRM (bool, true if use of Hear Rate Monitor), isOxSat (bool, true if use of O2Sat), isRR (bool, true if use of Respiratory Rate Monitor),
-			 conType[] (bool array, true if BLE, false if Wired, [0] = HRM Sensor, [1] = O2Sat Sensor, [2] = RR Sensor)
-
-			 Create an object while storing what the user will connect to the software
+			 @param[in] isHRM (bool, true if use of Hear Rate Monitor)
+			 @param[in] isOxSat (bool, true if use of O2Sat)
+			 @param[in] isRR (bool, true if use of Respiratory Rate Monitor),
+			 @param[in] conType[] (bool array, true if BLE, false if Wired, [0] = HRM Sensor, [1] = O2Sat Sensor, [2] = RR Sensor)
 		*/
-
 		Sensors(bool isHRM, bool isOxSat, bool isRR, bool conType[]);
 		~Sensors();
 
 		short int get_RawHRData();
 		short int get_RawOxSatData();
 		short int get_RawRRData();
+		std::vector<float> get_SampleData();
 		bool ready = false;
 	};
 #elif __APPLE__
--- vitalsMonitor/vitalsMonitor.cpp
+++ vitalsMonitor/vitalsMonitor.cpp
@@ -1,16 +1,37 @@
 #include <iostream>
-#include "Sensors.h" // TODO: Look to handle sensors differently
 #include <SDL.h>
 #include <SDL_main.h>
 #include <SDL_ttf.h>
 #include <SDL_image.h>
 #include <math.h>
+#include "Sensors.h" // TODO: Look to handle sensors differently
+#include "Graph.h"
 
+// TODO: REPLACE HARD CODED PARAMS BY CONFIG FILE PARAMS
+
+// SCREEN HARD CODED PARAMETERS
+enum
+{
+    SCREEN_W_SIZE = 1920,
+    SCREEN_H_SIZE = 1080
+};
+
+// GRAPH HARD CODED PARAMETERS
+enum
+{
+    GRAPH_W_SIZE = 1640,
+    GRAPH_H_SIZE = 250,
+    GRAPH_X_POS = 280,
+    GRAPH_Y_POS = 80
+};
+
+Graph* BPM = new Graph(GRAPH_W_SIZE, GRAPH_H_SIZE, GRAPH_X_POS, GRAPH_Y_POS, 100);
+Graph* Sp02 = new Graph(GRAPH_W_SIZE, GRAPH_H_SIZE, GRAPH_X_POS, GRAPH_Y_POS + GRAPH_H_SIZE, 100);
+Graph* RR = new Graph(GRAPH_W_SIZE, GRAPH_H_SIZE, GRAPH_X_POS, GRAPH_Y_POS + GRAPH_H_SIZE * 2, 100);
 
 /*
- * @brief Struct representing methods for drawing in the application.
- */
-
+* @brief Struct representing methods for drawing in the application.
+*/
 struct drawMethods {
     /*
      * @brief Draw text on the renderer.
@@ -24,20 +45,20 @@ struct drawMethods {
      * @param x The x-coordinate for the text position.
      * @param y The y-coordinate for the text position.
      */
-    void text(TTF_Font* font, const char *text, SDL_Color color, SDL_Renderer* renderer, int x, int y) {
+    static void text(TTF_Font* font, const char *text, SDL_Color color, SDL_Renderer* renderer, int x, int y) {
         // TEXTS
         SDL_Surface* textSurface = TTF_RenderText_Solid(font, text, color);
         SDL_Texture* textTexture = SDL_CreateTextureFromSurface(renderer, textSurface);
 
         // Get the dimensions of the text surface
-        int textWidth = textSurface->w;
-        int textHeight = textSurface->h;
+        const int textWidth = textSurface->w;
+        const int textHeight = textSurface->h;
 
         // Define the rendering position
-        SDL_Rect textRect = { x - textWidth / 2, y - textHeight / 2, textWidth, textHeight };
+        const SDL_Rect textRect = { x - textWidth / 2, y - textHeight / 2, textWidth, textHeight };
 
         // Render the text
-        SDL_RenderCopy(renderer, textTexture, NULL, &textRect);
+        SDL_RenderCopy(renderer, textTexture, nullptr, &textRect);
 
         // Free resources
         SDL_FreeSurface(textSurface);
@@ -59,7 +80,7 @@ struct drawMethods {
      * @param color The color of the line.
      * @param renderer The SDL renderer.
      */
-    void line(int x, int y, int xEnd, int yEnd, SDL_Color color, SDL_Renderer* renderer) {
+    static void line(int x, int y, int xEnd, int yEnd, SDL_Color color, SDL_Renderer* renderer) {
 
         // coords[2][2] = { {x,y},  // Start position
         //                  {x,y} } // End position
@@ -69,67 +90,18 @@ struct drawMethods {
     }
 };
 
-struct graph {
-    // Graph parameters
-    const int graphWidth = 1640;
-    const int graphHeight = 250;
-
-    const int bpm_xOffset = 280;
-    const int bpm_yOffset = 80;
-
-    const int Sp02_xOffset = 280;
-    const int Sp02_yOffset = 330;
-
-    const int RR_xOffset = 280;
-    const int RR_yOffset = 580;
-
-    // Real-time graph logic
-    double time = 0.0;
-    const double bpm_frequency = 0.01;
-    const double bpm_amplitude = 100;
-
-    const double Sp02_frequency = 0.01;
-    const double Sp02_amplitude = 100;
-
-    const double RR_frequency = 1.0;
-    const double RR_amplitude = 100;
-
-    void renderGraph(SDL_Renderer *renderer) {
-        
-
-
-        for (int x = 0; x < graphWidth; ++x) {
-
-            double bpm_y = bpm_amplitude * sin(bpm_frequency * time);
-            double Sp02_y = Sp02_amplitude * sin(Sp02_frequency * time);
-            double RR_y = RR_amplitude * sin(RR_frequency * time);
-
-            int bpm_graphY = graphHeight / 2 - (int)bpm_y + bpm_yOffset;
-            int Sp02_graphY = graphHeight / 2 - (int)Sp02_y + Sp02_yOffset;
-            int RR_graphY = graphHeight / 2 - (int)RR_y + RR_yOffset;
-
-            SDL_RenderDrawPoint(renderer, x + bpm_xOffset, bpm_graphY);
-            SDL_RenderDrawPoint(renderer, x + Sp02_xOffset, Sp02_graphY);
-            SDL_RenderDrawPoint(renderer, x + RR_xOffset, RR_graphY);
-
-        }
-
-        time += 0.1;
-    }
-
-};
-
-
-
 /*
-* @brief Proceed to starts the needed services like BLE, ...
-*
-* This function starts the process of Bluetooth Low Energy, creates Sensors objects, ...
-* It is needed before lauching the GUI
+* @brief Connecting to hardware sensors
+* @brief This function connects to the sensors and returns an object that can be used to get data stream from each sensors.
+* @brief It is mandatory before launching the UI.
+* @param None.
+* @throws HARDWARE_STARTUP_NOT_READY is thrown if the startup can't connect to the hardware in time
+* @throws THREADS_STARTUP_FAILURE is thrown if the startup procedure fails
+* @see Sensors Class
+* @return Returns an object that handles the hardware data stream.
+* @warning Useless for the moment
 */
-void startup() {
-    char buffer;
-
+Sensors startup() {
     std::cout << "******************************************************************\n";
     std::cout << "ECG, SpO2, Blood Pressure and Respiratory Rate monitoring software\n\n";
 
@@ -145,19 +117,17 @@ void startup() {
 
         if (hdw_Sensors.ready) {
             std::cout << "HARDWARE API " << "\033[1;32m" << "OK" << "\033[1;0m\n";
-        }
-        else {
-            throw std::runtime_error("HARDWARE STARTUP NOT READY");
+
+            // TODO: Check the data integrity 
+            std::cout << "WAITING FOR SENSORS\n\n";
+            std::cout << "\033[7;32mSTARTED UP\033[4;0m\n";
+
+            return hdw_Sensors;
+        } else {
+            throw std::runtime_error("HARDWARE_STARTUP_NOT_READY");
         }
 
         // TODO: MANAGE THREADS AND ASYNC FUNCTIONS IF USED (I might run on single thread (except SDL) if I can make that not too heavy)
-
-        if (true) {
-            std::cout << "THREADS " << "\033[1;32m" << "OK" << "\033[1;0m\n\n";
-        }
-        else {
-            throw std::runtime_error("THREADS STARTUP FAILURE");
-        }
     }
     catch (const std::exception& exc)
     {
@@ -165,14 +135,12 @@ void startup() {
         std::cout << "\033[7;31m" << "FATAL: " << "STARTUP FAILURE" << "\033[1;0m\n\n";
         std::cout << "\033[7;31m" << "FATAL: " << "Enter to quit the program..." << "\033[1;0m";
         std::cin.get();
-        throw exc;
+        throw;
     }
-
-    std::cout << "WAITING FOR SENSORS\n\n";
-    std::cout << "\033[7;32mSTARTED UP\033[4;0m\n";
 }
 
-int gui() {
+// TODO: Function needs more comments
+int gui(Sensors *sensors) {
     std::cout << "\033[1;32mSTARTING GUI\033[1;0m\n";
 
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
@@ -191,11 +159,11 @@ int gui() {
 
     // Create a window
     SDL_Window* window = SDL_CreateWindow(
-        "Health Monitoring Software",                       // Window title
+        "Health Monitoring Software",       // Window title
         SDL_WINDOWPOS_CENTERED,             // X position
         SDL_WINDOWPOS_CENTERED,             // Y position
-        1920,                               // Width
-        1080,                               // Height
+        SCREEN_W_SIZE,                      // Width
+        SCREEN_H_SIZE,                      // Height
         SDL_WINDOW_FULLSCREEN_DESKTOP       // Flags
     );
 
@@ -221,17 +189,12 @@ int gui() {
 
     // Open style font
     TTF_Font* font = TTF_OpenFont("OpenSans-Regular.ttf", 50);
-    
 
     // Main loop
     bool quit = false;
     SDL_Event event;
-
-    drawMethods draw;
-    graph graphVitals;
-
-
-
+    
+    const std::vector<float> data = sensors->get_SampleData(); // FOR TEST PURPOSES
 
     while (!quit) {
         // Handle events
@@ -247,81 +210,93 @@ int gui() {
         SDL_RenderClear(renderer);
 
         // LINES
-
-        draw.line(80, 80, 80, 1080, SDL_Color{ 255, 255, 255, 255 }, renderer); // H1 LINE
-        draw.line(280, 80, 280, 830, SDL_Color{ 255, 255, 255, 255 }, renderer); // H2 LINE
-        draw.line(725, 830, 725, 1080, SDL_Color{ 255, 255, 255, 255 }, renderer); // H3 LINE
-        draw.line(1500, 830, 1500, 1080, SDL_Color{ 255, 255, 255, 255 }, renderer); // H4 LINE
-        draw.line(0, 80, 1920, 80, SDL_Color{ 255, 255, 255, 255 }, renderer);  // W1 LINE
-        draw.line(80, 330, 1920, 330, SDL_Color{ 50, 125, 255, 255 }, renderer);  // W2 LINE
-        draw.line(80, 580, 1920, 580, SDL_Color{ 50, 125, 255, 255 }, renderer);  // W3 LINE
-        draw.line(80, 830, 1920, 830, SDL_Color{ 50, 125, 255, 255 }, renderer);  // W4 LINE
+        // TODO: Stop using magic numbers
+        
+        drawMethods::line(80, 80, 80, 1080, SDL_Color{ 255, 255, 255, 255 }, renderer); // H1 LINE
+        drawMethods::line(280, 80, 280, 830, SDL_Color{ 255, 255, 255, 255 }, renderer); // H2 LINE
+        drawMethods::line(725, 830, 725, 1080, SDL_Color{ 255, 255, 255, 255 }, renderer); // H3 LINE
+        drawMethods::line(1500, 830, 1500, 1080, SDL_Color{ 255, 255, 255, 255 }, renderer); // H4 LINE
+        drawMethods::line(0, 80, 1920, 80, SDL_Color{ 255, 255, 255, 255 }, renderer);  // W1 LINE
+        drawMethods::line(80, 330, 1920, 330, SDL_Color{ 50, 125, 255, 255 }, renderer);  // W2 LINE
+        drawMethods::line(80, 580, 1920, 580, SDL_Color{ 50, 125, 255, 255 }, renderer);  // W3 LINE
+        drawMethods::line(80, 830, 1920, 830, SDL_Color{ 50, 125, 255, 255 }, renderer);  // W4 LINE
 
         // Draw Text
-
-        draw.text(font, "HR", SDL_Color{255, 0, 0, 255}, renderer, 120, 110);
-
-        TTF_SetFontSize(font, 100);
-        draw.text(font, "00", SDL_Color{ 255, 255, 255, 255 }, renderer, 180, 205);
-        TTF_SetFontSize(font, 50);
-
-        draw.text(font, "Sp02", SDL_Color{ 0, 0, 255, 255 }, renderer, 150, 360);
+        // TODO: Stop using magic numbers
+        
+        drawMethods::text(font, "HR", SDL_Color{255, 0, 0, 255}, renderer, 120, 110);
 
         TTF_SetFontSize(font, 100);
-        draw.text(font, "00", SDL_Color{ 255, 255, 255, 255 }, renderer, 180, 455);
+        drawMethods::text(font, "00", SDL_Color{ 255, 255, 255, 255 }, renderer, 180, 205);
         TTF_SetFontSize(font, 50);
 
-        draw.text(font, "RR", SDL_Color{ 0, 255, 0, 255 }, renderer, 120, 610);
+        drawMethods::text(font, "Sp02", SDL_Color{ 0, 0, 255, 255 }, renderer, 150, 360);
 
         TTF_SetFontSize(font, 100);
-        draw.text(font, "00", SDL_Color{ 255, 255, 255, 255 }, renderer, 180, 705);
+        drawMethods::text(font, "00", SDL_Color{ 255, 255, 255, 255 }, renderer, 180, 455);
         TTF_SetFontSize(font, 50);
 
-        draw.text(font, "Sys (mmHg)", SDL_Color{ 120, 200, 200, 255 }, renderer, 230, 880);
+        drawMethods::text(font, "RR", SDL_Color{ 0, 255, 0, 255 }, renderer, 120, 610);
 
         TTF_SetFontSize(font, 100);
-        draw.text(font, "000", SDL_Color{ 255, 255, 255, 255 }, renderer, 225, 975);
-        draw.text(font, "/", SDL_Color{ 255, 255, 255, 255 }, renderer, 390, 975);
+        drawMethods::text(font, "00", SDL_Color{ 255, 255, 255, 255 }, renderer, 180, 705);
         TTF_SetFontSize(font, 50);
 
-        draw.text(font, "Dia (mmHg)", SDL_Color{ 120, 200, 200, 255 }, renderer, 550, 880);
+        drawMethods::text(font, "Sys (mmHg)", SDL_Color{ 120, 200, 200, 255 }, renderer, 230, 880);
 
         TTF_SetFontSize(font, 100);
-        draw.text(font, "00", SDL_Color{ 255, 255, 255, 255 }, renderer, 550, 975);
+        drawMethods::text(font, "000", SDL_Color{ 255, 255, 255, 255 }, renderer, 225, 975);
+        drawMethods::text(font, "/", SDL_Color{ 255, 255, 255, 255 }, renderer, 390, 975);
         TTF_SetFontSize(font, 50);
 
-        draw.text(font, "Body Temp.", SDL_Color{ 120, 200, 200, 255 }, renderer, 1650, 880);
+        drawMethods::text(font, "Dia (mmHg)", SDL_Color{ 120, 200, 200, 255 }, renderer, 550, 880);
 
         TTF_SetFontSize(font, 100);
-        draw.text(font, "00", SDL_Color{ 255, 255, 255, 255 }, renderer, 1650, 975);
-        draw.text(font, "°C", SDL_Color{ 255, 255, 255, 255 }, renderer, 1775, 975);
+        drawMethods::text(font, "00", SDL_Color{ 255, 255, 255, 255 }, renderer, 550, 975);
         TTF_SetFontSize(font, 50);
 
-        // Render graphs 
+        drawMethods::text(font, "Body Temp.", SDL_Color{ 120, 200, 200, 255 }, renderer, 1650, 880);
 
-        graphVitals.renderGraph(renderer);
+        TTF_SetFontSize(font, 100);
+        drawMethods::text(font, "00", SDL_Color{ 255, 255, 255, 255 }, renderer, 1650, 975);
+        drawMethods::text(font, "\u00B0C", SDL_Color{ 255, 255, 255, 255 }, renderer, 1775, 975);
+        TTF_SetFontSize(font, 50);
 
-        // Show frame
-        SDL_RenderPresent(renderer);
+        // Render graphs
 
-        // Delay to control frame rate
-        SDL_Delay(16);  // Assuming 60 FPS
+        for (int index = 0; index < static_cast<int>(data.size()); index++) {
+            BPM->setValue(data[index], index);
+            BPM->renderGraph(renderer);
+
+            Sp02->setValue(data[index], index);
+            Sp02->renderGraph(renderer);
+            
+            RR->setValue(data[index], index);
+            RR->renderGraph(renderer);
+            
+            // Delay to control frame rate
+            SDL_Delay(8);
+
+            // Show frame
+            SDL_RenderPresent(renderer);
+        }
     }
 
     // Destroy font, renderer, window, and quit SDL_ttf and SDL
-
     TTF_CloseFont(font);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     TTF_Quit();
     SDL_Quit();
+
+    return 0;
 }
 
 /*
- * @brief Entry point of the program
- */
+* @brief Entry point of the program
+*/
 int main(int argc, char* argv[])
 {
-    startup();
-    return gui();
+    Sensors sensors = startup();
+    return gui(&sensors);
 }
